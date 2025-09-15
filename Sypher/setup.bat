ed2e6fe7f4cf04920c340b39572bf99bb88f54c3
Ryōka <SinLimerence@proton.me>
2025-09-15 19:56:39 +0200

    Add files via upload

---
+++ Sypher/setup.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Installing required Python modules...
+pip install discord.py pyautogui keyboard opencv-python requests pyperclip psutil comtypes
+echo.
+echo All dependencies installed!
+pause
